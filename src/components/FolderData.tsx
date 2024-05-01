2e1e4e160faced153710887233c783f66ddafd8e
swarnpallavIN074 <swarn.bhaskar@toothsi.in>
2024-05-01 19:40:45 +0530

    feat:  initial commit

---
+++ src/components/FolderData.tsx
@@ -0,0 +1,89 @@
+import React, { useMemo } from "react";
+import useFileManagement from "../hooks/useFileManagement";
+import { Types } from "../context/FolderDataContext";
+import { FaFileAlt, FaFolder } from "react-icons/fa";
+import { formatRelative, subDays } from "date-fns";
+import { BiFileBlank } from "react-icons/bi";
+import Edit from "./Edit";
+
+const FolderData = () => {
+  const { getFolders, getCurrentFolderId, updateCurrentFolderId } =
+    useFileManagement();
+
+  const folders: any = getFolders();
+
+  const currentFolderId = getCurrentFolderId();
+
+  const currentFolderData: any = useMemo(() => {
+    let currentData: any;
+    const getData = (obj: any) => {
+      if (obj.type === Types.FILE) {
+        return;
+      }
+      if (obj.type === Types.FOLDER && obj.id === currentFolderId) {
+        currentData = obj;
+        return;
+      }
+
+      for (let i = 0; i < obj.contents.length; i++) {
+        const currentObject = obj.contents[i];
+        getData(currentObject);
+        if (currentData) {
+          return;
+        }
+      }
+    };
+
+    getData(folders[0]);
+    return currentData;
+  }, [folders, currentFolderId]);
+
+  return currentFolderData.contents.length ? (
+    <div className="folderData">
+      <div className="folderDataHeader">
+        <div>Name</div>
+        <div>Last Modified</div>
+        <div>Size</div>
+      </div>
+      <div>
+        {currentFolderData?.contents?.map((data: any) => {
+          return (
+            <div className="folderDataHeader" key={data.id}>
+              <div
+                className="nameColumn"
+                onDoubleClick={() => {
+                  if (data.type === Types.FOLDER) {
+                    updateCurrentFolderId(data.id);
+                  }
+                }}
+              >
+                {data.type === Types.FOLDER ? (
+                  <FaFolder size={24} />
+                ) : (
+                  <FaFileAlt size={24} />
+                )}
+                {data.name}
+              </div>
+              <div>
+                {data.lastModified &&
+                  formatRelative(
+                    subDays(new Date(), 0),
+                    new Date(data.lastModified)
+                  )}
+              </div>
+              <div>{data.size ? `${data.size / 1000} KB` : "-"}</div>
+              <Edit id={data.id} name={data.name} type={data.type} />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  ) : (
+    <div className="empty">
+      <BiFileBlank size={80} />
+      <div>Empty Folder</div>
+    </div>
+  );
+};
+
+export default FolderData;
---
+++ src/components/UploadButton.tsx
@@ -0,0 +1,109 @@
+import { useRef, useState } from "react";
+import Modal from "@mui/material/Modal";
+import { MdCreateNewFolder } from "react-icons/md";
+import { FaFileUpload } from "react-icons/fa";
+import useFileManagement from "../hooks/useFileManagement";
+import { Types } from "../context/FolderDataContext";
+import { Button, Input } from "@mui/material";
+
+// TODO: update current folder's lastModify
+
+const UploadButton = () => {
+  const [open, setOpen] = useState(false);
+  const [nameModal, setNameModal] = useState(false);
+  const [newFolderName, setNewFolderName] = useState("Untitled folder");
+  const fileUploadRef = useRef<any>();
+
+  const { addFile, addFolder } = useFileManagement();
+  const handleFileUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+    addFile({
+      id: Date.now() + "",
+      type: Types.FILE,
+      fileType: fileUploadRef.current.files[0].type,
+      name: fileUploadRef.current.files[0].name,
+      size: fileUploadRef.current.files[0].size,
+      lastModified: fileUploadRef.current.files[0].lastModified,
+    });
+    setOpen(false);
+  };
+
+  const handleNewFolder = () => {
+    addFolder({
+      id: Date.now() + "",
+      type: Types.FOLDER,
+      name: newFolderName,
+      lastModified: Date.now(),
+      contents: [],
+    });
+    setNameModal(false);
+    setNewFolderName("Untitled folder");
+  };
+
+  return (
+    <>
+      <div className="newBtnWrapper">
+        <button onClick={() => setOpen(true)}>
+          <span className="plus-icon">+</span>
+          <span>New</span>
+        </button>
+      </div>
+      <Modal className="muiModal" open={open} onClose={() => setOpen(false)}>
+        <div className="uploadModal">
+          <div
+            className="uploadItem"
+            onClick={() => {
+              setNameModal(true);
+              setOpen(false);
+            }}
+          >
+            <MdCreateNewFolder size={40} />
+            New Folder
+          </div>
+          <div
+            onClick={() => {
+              fileUploadRef.current.click();
+            }}
+            className="uploadItem"
+          >
+            <input
+              ref={fileUploadRef}
+              hidden
+              type="file"
+              onChange={handleFileUpload}
+            />
+            <FaFileUpload size={40} />
+            File Upload
+          </div>
+        </div>
+      </Modal>
+      <Modal
+        className="muiModal"
+        open={nameModal}
+        onClose={() => setNameModal(false)}
+      >
+        <div className="uploadModal">
+          <div className={"modalHeader"}>New Folder</div>
+          <div className="uploadItem">
+            <Input
+              type="text"
+              value={newFolderName}
+              onChange={(e) => setNewFolderName(e.target.value)}
+            />
+          </div>
+          <div className="modalFooter">
+            <Button
+              onClick={handleNewFolder}
+              disabled={!newFolderName}
+              variant="outlined"
+            >
+              Create
+            </Button>
+            <Button onClick={() => setNameModal(false)}>Cancel</Button>
+          </div>
+        </div>
+      </Modal>
+    </>
+  );
+};
+
+export default UploadButton;
